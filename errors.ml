a7b2ea5ee2e23cb9e9bc0bf61b5f014eb999d9a7
msfukui <msfukui@gmail.com>
2019-03-23 21:27:05 +0900

    6.1 の解を追加。

---
+++ errors.ml
@@ -0,0 +1,13 @@
+(* 6.1 (1) *)
+let square x = x * x
+square 3
+
+(* 6.1 (2) *)
+let pi = 3.14 ;
+let circle r = 2. *. pi *. r
+circle 2.
+
+(* 6.1 (3) *)
+let TV bangumi youbi =
+  bangumi ^ "は" ^ youbi ^ "に放映です。"
+TV "ポケモン" "木曜日"
